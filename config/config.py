6828c2510c6c5f9c1e6cb9f7d2b0b1b5201b6b49
赵巍 <2426595849@qq.com>
2018-06-20 16:24:37 +0800

    zww

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ class SphinxConfig(object):
     Sphinx_Config = {
         'host':'127.0.0.1',
         'port': 9312,
-        'db': 'pan',
+        'db': '#',
         'p':15
     }
 '''
@@ -14,11 +14,11 @@ mongodb 配置文件
 '''
 class MongoConfig(object):
     Mongo_Config = {
-        'host':'47.105.45.25',
+        'host':'127.0.0.1',
         'port': 27017,
-        'db': 'pan',
-        'name':"root",
-        'pwd':"zhaowei123"
+        'db': '#',
+        'name':"#",
+        'pwd':"#"
     }
 
 '''
@@ -26,11 +26,11 @@ mysql 配置文件
 '''
 class MysqlConfig(object):
     Mysql_Config = {
-        'host':'47.105.45.25',
+        'host':'127.0.0.1',
         'port': 3306,
-        'db': 'pan',
-        'name':"root",
-        'pwd':"zhaowei123"
+        'db': '#',
+        'name':"#",
+        'pwd':"#"
     }
 '''
 登录证书
