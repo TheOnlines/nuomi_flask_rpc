4ae41bc44a0f11480e1a15530cfc14f79dd0cb37
赵巍 <2426595849@qq.com>
2018-06-20 16:23:03 +0800

    zww

--- config/config.py
+++ config/config.py
@@ -6,7 +6,7 @@ class SphinxConfig(object):
     Sphinx_Config = {
         'host':'127.0.0.1',
         'port': 9312,
-        'db': '#',
+        'db': 'pan',
         'p':15
     }
 '''
@@ -14,11 +14,11 @@ mongodb 配置文件
 '''
 class MongoConfig(object):
     Mongo_Config = {
-        'host':'127.0.0.1',
+        'host':'47.105.45.25',
         'port': 27017,
-        'db': '#',
-        'name':"#",
-        'pwd':"#"
+        'db': 'pan',
+        'name':"root",
+        'pwd':"zhaowei123"
     }
 
 '''
@@ -26,16 +26,16 @@ mysql 配置文件
 '''
 class MysqlConfig(object):
     Mysql_Config = {
-        'host':'127.0.0.1',
+        'host':'47.105.45.25',
         'port': 3306,
-        'db': '#',
-        'name':"#",
-        'pwd':"#"
+        'db': 'pan',
+        'name':"root",
+        'pwd':"zhaowei123"
     }
 '''
 登录证书
 '''
 class Credentials(object):
     Creden_Tials = {
-        'zhaowei':"#"
+        'zhaowei':"zhaowei123"
     }
